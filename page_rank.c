c0772a86de6309bab3a59db235f709502e30841a
Naimbessaha4 <naim.bessaha@ens.uvsq.fr>
2022-12-06 21:30:55 +0100

    Page Rank V2

--- page_rank.c
+++ page_rank.c
@@ -1,28 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <blas.h>
+#include "blas.h"
+
+
+/*
+=================================================================
+Lecture de la matrice
+
+*/
+double *Lecture_Matrice(int nb_ligne, int nb_colone, FILE *matrice)
+{
+
+    double *mat = malloc(nb_ligne * nb_colone * sizeof(double));
+
+    for (int i = 0; i < nb_ligne; i++)
+    {
+        for (int j = 0; j < nb_colone; j++)
+        {
+            double v = 0;
+            fscanf(matrice, "%lf", &v);
+
+            mat[j * nb_ligne + i] = v;
+        }
+    }
+
+    return mat;
+}
+
+/*
+===========================================================================
+affichage de la matrice
+
+*/
+
+
 
 
 double *page_rank(double *A, int n, double B, double p){
 
-    double *x=malloc(n * sizeof(double));
+    double *x=calloc(n , sizeof(double));
 
-    double *x_new=malloc(n * sizeof(double));
+    double *x_new=calloc(n , sizeof(double));
 
+
+    
     for (int j=0; j<n; j++){
         x_new[j]=1./n;
     }
 
-    int i =0;
+    double *Z = transposeMatrix(n,n,A);
+
+    
+  
+
+    
 
     for (;;){
         
-        Matrix_Vector_Product(A, x_new, n,n , x);
+        Matrix_Vector_Product(Z, x_new, n,n , x);
+
+        displayVector(x, n);
 
 
         for(int i = 0; i<n; i++){
-            x[i]=B * x[i] + (1-B)/n ;
+            x[i]=B * x[i];
+             x[i]+= (1-B)/n ;
         }
+
+        displayVector(x, n);
+
+
         double norm = Norme(x, n);
 
         for(int i = 0; i<n; i++){
@@ -46,11 +93,13 @@ double *page_rank(double *A, int n, double B, double p){
         for ( int i = 0 ; i<n; i++){
             x_new[i] = x[i]; 
         }
+   
 
 
         
     }
-
+    free(x_new);
+    free(Z);
 
 
 
@@ -60,6 +109,28 @@ double *page_rank(double *A, int n, double B, double p){
 
 
 int main(int argc, char* argv){
+    FILE *mat;
+    mat = fopen("Deezer-small-DS/Exo Td/transition_matrix.csv", "r");
+    if (mat == NULL)
+    {
+        printf("le fichier n'existe pas");
+        return 1;
+    }
+
+    double *mat1 = Lecture_Matrice(4, 4, mat);
+    fclose(mat);
+
+
+
+    double *x=page_rank(mat1, 4, 0.85, 0.001);
+
+    displayVector(x, 4);
+
+    free(mat1);
+    free(x);
+
+    
+
    
     return 0;
 }
