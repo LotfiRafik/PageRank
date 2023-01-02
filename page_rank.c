f474d6193551f78138af16c18274975161d24753
Bouchafa Lotfi Rafik <gl_bouchafa@esi.dz>
2023-01-02 10:30:33 +0100

    feat generate teleportation vector for personalized pagerank

--- page_rank.c
+++ page_rank.c
@@ -3,22 +3,40 @@
 #include "blas.h"
 #include "page_rank.h"
 #include <string.h>
+#include <time.h>
 
 double *page_rank(double *A, int nbNonZeroA , int n, double B, double p, int parallel_mode, int sparce_rep){
 
     double *x=calloc(n , sizeof(double)),
             *x_prec=calloc(n , sizeof(double)),
-             *teleportation_vector=calloc(n , sizeof(double)),
-             error;
+            *teleportation_vector=calloc(n , sizeof(double)),
+            error;
+
+    int sum_teleportation_vector = 0;
+
+
+    srand(time(NULL));   // Initialization, should only be called once.
+
+    for (size_t i = 0; i < n; i++)
+    {
+        int r = rand() % 10 + 1;    // Returns a pseudo-random integer between 1 and 10.
+        sum_teleportation_vector += r;
+        teleportation_vector[i] = r;
+    }
+    for (size_t i = 0; i < n; i++)
+    {
+        teleportation_vector[i] /= sum_teleportation_vector; 
+    }
+
 
     /* TODO 
         create parallel function to initialize a vector with a scalar value
     */
     // initializer le vecteur de depart
     for (int j=0; j<n; j++){
-        x[j]=1./n;
-        // TODO Personalized pageRank changes this vector
-        teleportation_vector[j] = 1./n;
+        x[j] = 1./n;
+        // Old implementation 
+        // teleportation_vector[j] = 1./n;
     }
 
     // i represente le nombre d'itirations
